2b19e6e1d9c9e0be94b59e0101cbe3f32750be95
ltdainstituicao-dotcom <ltdainstituicao@gmail.com>
2025-08-08 02:22:53 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+// Substitua com suas credenciais reais do Firebase
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "seu-projeto.firebaseapp.com",
+    projectId: "seu-projeto",
+    storageBucket: "seu-projeto.appspot.com",
+    messagingSenderId: "000000000000",
+    appId: "1:000000000000:web:xxxxxxxxxxxx"
+};
+
+firebase.initializeApp(firebaseConfig);
